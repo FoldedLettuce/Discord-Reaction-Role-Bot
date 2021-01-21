a1dca633fcb7766451afa56e683d55cd5c3f88f3
LiamMoroney <45380013+LiamMoroney@users.noreply.github.com>
2021-01-21 15:57:42 -0600

    Add files via upload
    Main Files

---
+++ main.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+
+const prefix = '.';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require (`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+
+client.once('ready', () => {
+    console.log(`Notona is online!`);
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift() .toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args, Discord, client);
+    }  else if (command == 'reactionrole') {
+        client.commands.get('reactionrole').execute(message, args, Discord, client);
+    }  else if (command == 'rulesreaction') {
+        client.commands.get('rulesreaction').execute(message, args, Discord, client);
+    }
+});
+
+
+client.login('ODAwODYxMjIzNDI3MjQ0MDQy.YAYSIg.eFbE_JCzANhSmwryS2-J3KHbKuQ')
